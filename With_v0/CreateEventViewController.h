7695e9a5d5aae64d17f258bb90fd484d139794ed
vikdenic <vik.denic@gmail.com>
2014-06-15 03:08:42 -0500

    initial commit

---
+++ With_v0/CreateEventViewController.h
@@ -0,0 +1,13 @@
+//
+//  CreateEventViewController.h
+//  With_v0
+//
+//  Created by Vik Denic on 6/15/14.
+//  Copyright (c) 2014 Mobile Makers. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CreateEventViewController : UIViewController
+
+@end
--- With_v0/HomeViewController.h
+++ With_v0/HomeViewController.h
@@ -1,13 +1,13 @@
 //
-//  ViewController.h
+//  HomeViewController.h
 //  With_v0
 //
-//  Created by Vik Denic on 6/14/14.
+//  Created by Vik Denic on 6/15/14.
 //  Copyright (c) 2014 Mobile Makers. All rights reserved.
 //
 
 #import <UIKit/UIKit.h>
 
-@interface ViewController : UIViewController
+@interface HomeViewController : UIViewController
 
 @end
