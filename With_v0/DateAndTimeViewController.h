39939662c1429c8c8c10984638314edfb76eeac7
vikdenic <vik.denic@gmail.com>
2014-06-21 19:52:58 -0500

    create DateAndTimeVC and in Storyboard

---
+++ With_v0/DateAndTimeViewController.h
@@ -0,0 +1,13 @@
+//
+//  DateAndTimeViewController.h
+//  With_v0
+//
+//  Created by Vik Denic on 6/21/14.
+//  Copyright (c) 2014 Mobile Makers. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DateAndTimeViewController : UIViewController
+
+@end
---
+++ With_v0/DateAndTimeViewController.m
@@ -0,0 +1,34 @@
+//
+//  DateAndTimeViewController.m
+//  With_v0
+//
+//  Created by Vik Denic on 6/21/14.
+//  Copyright (c) 2014 Mobile Makers. All rights reserved.
+//
+
+#import "DateAndTimeViewController.h"
+
+@interface DateAndTimeViewController ()
+@property (weak, nonatomic) IBOutlet UIDatePicker *datePicker;
+
+@end
+
+@implementation DateAndTimeViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+}
+
+
+-(void)viewWillDisappear:(BOOL)animated
+{
+    NSDate *myDate = self.datePicker.date;
+
+    NSDateFormatter *dateFormat = [[NSDateFormatter alloc] init];
+    [dateFormat setDateFormat:@"cccc, MMM d, hh:mm aa"];
+    NSString *prettyVersion = [dateFormat stringFromDate:myDate];
+    NSLog(@"%@", prettyVersion);
+}
+
+@end
