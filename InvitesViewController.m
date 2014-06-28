14161205b6775bfaa942ce52fb324c0e731868e0
mitch2be@gmail.com <mitch2be@gmail.com>
2014-06-28 18:52:54 -0500

    Getting Storyboard

--- InvitesViewController.m
+++ InvitesViewController.m
@@ -7,8 +7,12 @@
 //
 
 #import "InvitesViewController.h"
+#import <Parse/Parse.h>
 
-@interface InvitesViewController ()
+@interface InvitesViewController () <UITableViewDataSource, UITableViewDelegate>
+
+@property NSMutableArray *eventArray;
+@property (weak, nonatomic) IBOutlet UITableView *tableView;
 
 @end
 
@@ -17,6 +21,25 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
+
+    self.eventArray = [NSMutableArray array];
+}
+
+- (void)queryForEvents
+{
+    PFQuery *query = [PFQuery queryWithClassName:@"EventInvite"];
+    [query whereKey:@"toUser" equalTo:[PFUser currentUser]];
+    [query includeKey:@"event"];
+    [query findObjectsInBackgroundWithBlock:^(NSArray *objects, NSError *error)
+     {
+         //this is the users events
+         for (PFObject *object in objects)
+         {
+             PFObject *event = [object objectForKey:@"event"];
+             [self.eventArray addObject:event];
+         }
+         [self.tableView reloadData];
+     }];
 }
 
 
