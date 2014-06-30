3033f28fa9fb5571556d23be6d3deb175a33f35e
mitch2be@gmail.com <mitch2be@gmail.com>
2014-06-30 09:31:06 -0500

    Camera View Controller

---
+++ With_v0/StreamCameraViewController.h
@@ -0,0 +1,13 @@
+//
+//  StreamCameraViewController.h
+//  With_v0
+//
+//  Created by Blake Mitchell on 6/30/14.
+//  Copyright (c) 2014 Mobile Makers. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface StreamCameraViewController : UIViewController
+
+@end
---
+++ With_v0/StreamCameraViewController.m
@@ -0,0 +1,23 @@
+//
+//  StreamCameraViewController.m
+//  With_v0
+//
+//  Created by Blake Mitchell on 6/30/14.
+//  Copyright (c) 2014 Mobile Makers. All rights reserved.
+//
+
+#import "StreamCameraViewController.h"
+
+@interface StreamCameraViewController ()
+
+@end
+
+@implementation StreamCameraViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+}
+
+
+@end
