78b0a1b6ffcd06df9843473cdb9f795a8fbe3f0d
mitch2be@gmail.com <mitch2be@gmail.com>
2014-07-01 14:34:41 -0500

    chat working

---
+++ ChatEventObject.m
@@ -0,0 +1,13 @@
+//
+//  ChatEventObject.m
+//  With_v0
+//
+//  Created by Blake Mitchell on 7/1/14.
+//  Copyright (c) 2014 Mobile Makers. All rights reserved.
+//
+
+#import "ChatEventObject.h"
+
+@implementation ChatEventObject
+
+@end
