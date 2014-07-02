330871e2cbc77a0b87e319183871502190bebb84
vikdenic <vik.denic@gmail.com>
2014-07-02 14:21:35 -0500

    crop photo fixed

--- StreamEventViewController.m
+++ StreamEventViewController.m
@@ -327,6 +327,7 @@
          {
              UIImage *temporaryImage = [UIImage imageWithData:data];
              cell.theImageView.image = temporaryImage;
+//             cell.theImageView.contentMode = UIViewContentModeScaleAspectFit;
          }
      }];
 
@@ -474,7 +475,8 @@
 
     } else {
 
-        [self presentModalViewController:self.imagePicker.imagePickerController animated:YES];
+//        [self presentModalViewController:self.imagePicker.imagePickerController animated:YES];
+        [self presentViewController:self.imagePicker.imagePickerController animated:YES completion:nil];
     }
 }
 
@@ -545,14 +547,61 @@
     [picker dismissViewControllerAnimated:YES completion:^{
 
     UIImage *image = [info valueForKey:UIImagePickerControllerOriginalImage];
+//
+//    CGSize sacleSize = CGSizeMake(320, 320);
+//    UIGraphicsBeginImageContextWithOptions(sacleSize, NO, 0.0);
+//    [image drawInRect:CGRectMake(0, 0, sacleSize.width, sacleSize.height)];
+//    UIImage * resizedImage = UIGraphicsGetImageFromCurrentImageContext();
+//    UIGraphicsEndImageContext();
 
-    CGSize sacleSize = CGSizeMake(320, 320);
-    UIGraphicsBeginImageContextWithOptions(sacleSize, NO, 0.0);
-    [image drawInRect:CGRectMake(0, 0, sacleSize.width, sacleSize.height)];
-    UIImage * resizedImage = UIGraphicsGetImageFromCurrentImageContext();
-    UIGraphicsEndImageContext();
+        CGSize size = image.size;
 
-    NSData *imageData = UIImageJPEGRepresentation(resizedImage, 0.05f);
+        // Crop the crop rect that the user selected
+        CGRect cropRect = [[info objectForKey:UIImagePickerControllerCropRect]
+                           CGRectValue];
+
+        // Create a graphics context of the correct size
+        UIGraphicsBeginImageContext(cropRect.size);
+        CGContextRef context = UIGraphicsGetCurrentContext();
+
+        // Correct for image orientation
+        UIImageOrientation orientation = [image imageOrientation];
+        if(orientation == UIImageOrientationUp) {
+            CGContextTranslateCTM(context, 0, size.height);
+            CGContextScaleCTM(context, 1, -1);
+            cropRect = CGRectMake(cropRect.origin.x,
+                                  -cropRect.origin.y,
+                                  cropRect.size.width,
+                                  cropRect.size.height);
+        } else if(orientation == UIImageOrientationRight) {
+            CGContextScaleCTM(context, 1.0, -1.0);
+            CGContextRotateCTM(context, -M_PI/2);
+            size = CGSizeMake(size.height, size.width);
+            cropRect = CGRectMake(cropRect.origin.y,
+                                  cropRect.origin.x,
+                                  cropRect.size.height,
+                                  cropRect.size.width);
+        } else if(orientation == UIImageOrientationDown) {
+            CGContextTranslateCTM(context, size.width, 0);
+            CGContextScaleCTM(context, -1, 1);
+            cropRect = CGRectMake(-cropRect.origin.x,
+                                  cropRect.origin.y,
+                                  cropRect.size.width,
+                                  cropRect.size.height);
+        }
+
+        // Draw the image in the correct place
+        CGContextTranslateCTM(context, -cropRect.origin.x, -cropRect.origin.y);
+        CGContextDrawImage(context,
+                           CGRectMake(0,0, size.width, size.height),
+                           image.CGImage);
+
+        // Pull out the cropped image
+        UIImage *imageTaken = UIGraphicsGetImageFromCurrentImageContext();
+        UIGraphicsEndImageContext();
+
+
+    NSData *imageData = UIImageJPEGRepresentation(imageTaken, 0.05f);
     PFFile *imageFile = [PFFile fileWithData:imageData];
     [imageFile saveInBackgroundWithBlock:^(BOOL succeeded, NSError *error)
          {
@@ -630,190 +679,3 @@
 
 @end
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-//- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
-//{
-//    StreamTableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"Cell"];
-    //    cell.theImageView.tag = indexPath.section;
-    //    cell.likeButton.tag = indexPath.section;
-    //    cell.likedImageView.hidden = YES;
-    //    cell.commentButton.tag = indexPath.section;
-    //
-    //    //getting the number of likes for each photo
-    //    PFQuery *query = [PFQuery queryWithClassName:@"LikeActivity"];
-    //    PFObject *object = [self.pictureAndVideoArray objectAtIndex:indexPath.section];
-    //    [query whereKey:@"photo" equalTo:object];
-    //    [query includeKey:@"fromUser"];
-    //
-    //    [query findObjectsInBackgroundWithBlock:^(NSArray *objects, NSError *error)
-    //     {
-    //
-    //         for (PFObject *object in objects)
-    //         {
-    //             NSString *objectID = [[object objectForKey:@"fromUser"] objectId];
-    //             NSString *currentUserID = [[PFUser currentUser] objectId];
-    //
-    //             if ([currentUserID isEqualToString:objectID])
-    //             {
-    //                 //this means user likes the photo
-    //                 UIImage *btnImage = [UIImage imageNamed:@"like_selected"];
-    //                 [cell.likeButton setImage:btnImage forState:UIControlStateNormal];
-    //                 cell.ifLiked = YES;
-    //
-    //
-    //             } else {
-    //                 UIImage *btnImage = [UIImage imageNamed:@"like_unselected"];
-    //                 [cell.likeButton setImage:btnImage forState:UIControlStateNormal];
-    //                 cell.ifLiked = NO;
-    //             }
-    //         }
-    //
-    //
-    //          cell.numberOfLikesLabel.text = [NSString stringWithFormat:@"%lu likes", (unsigned long)objects.count];
-    //     }];
-    //
-    //    PFFile *file = [object objectForKey:@"photo"];
-    //
-    //    [file getDataInBackgroundWithBlock:^(NSData *data, NSError *error)
-    //     {
-    //         if (!error)
-    //         {
-    //             UIImage *temporaryImage = [UIImage imageWithData:data];
-    //             cell.theImageView.image = temporaryImage;
-    //         }
-    //         else {
-    //             NSLog(@"Alert!");
-    //         }
-    //     }];
-    //
-    //    //double tap to like
-    //    if (cell.theImageView.gestureRecognizers.count == 0)
-    //    {
-    //        UITapGestureRecognizer *tapping = [[UITapGestureRecognizer alloc]initWithTarget:self action:@selector(tapTap:)];
-    //        tapping.numberOfTapsRequired = 2;
-    //        [cell.theImageView addGestureRecognizer:tapping];
-    //        cell.theImageView.userInteractionEnabled = YES;
-    //    }
-    //    cell.selectionStyle = UITableViewCellSelectionStyleNone;
-    
-//    return cell;
-//}
-
-//-(UIView *)tableView:(UITableView *)tableView viewForHeaderInSection:(NSInteger)section
-//{
-//    CGRect frame = tableView.frame;
-//
-//    PFObject *object = [self.pictureAndVideoArray objectAtIndex:section];
-//    PFUser *user = [object objectForKey:@"photographer"];
-//
-//
-//    //setting time top right
-//
-//    NSDate *timeOfPicture = [object valueForKey:@"createdAt"];
-//
-//    int seconds = -(int)[timeOfPicture timeIntervalSinceNow];
-//    int minutes = seconds/60;
-//
-//   UILabel *timeInterval = [[UILabel alloc] initWithFrame:CGRectMake(230, 5, 100, 30)];
-//    timeInterval.textColor = [UIColor whiteColor];
-//
-//        if (minutes < 60)
-//        {
-//            timeInterval.text = [NSString stringWithFormat:@"%im", minutes];
-//
-//        } else if (minutes > 60 && minutes < 1440)
-//        {
-//            minutes = minutes/60;
-//
-//            timeInterval.text = [NSString stringWithFormat:@"%ih", minutes];
-//
-//        } else {
-//
-//            minutes = minutes/1440;
-//            timeInterval.text = [NSString stringWithFormat:@"%id", minutes];
-//        }
-//
-//    //setting the username
-//    UILabel *title = [[UILabel alloc] initWithFrame:CGRectMake(50, 5, 100, 30)];
-//    title.text = [NSString stringWithFormat:@"%@", user.username];
-//    title.textColor = [UIColor whiteColor];
-//
-//    PFFile *userProfilePhoto = [user objectForKey:@"userProfilePhoto"];
-//    UIImageView *customImageView = [[UIImageView alloc] initWithFrame:CGRectMake(10, 5, 30, 30)];
-//    [userProfilePhoto getDataInBackgroundWithBlock:^(NSData *data, NSError *error)
-//     {
-//         if (!error)
-//         {
-//             UIImage *image = [UIImage imageWithData:data];
-//             customImageView.image = image;
-//             customImageView.layer.cornerRadius = customImageView.bounds.size.width/2;
-//             customImageView.layer.borderColor = [[UIColor colorWithRed:202/255.0 green:250/255.0 blue:53/255.0 alpha:1] CGColor];
-//             customImageView.layer.borderWidth = 2.0;
-//             customImageView.layer.masksToBounds = YES;
-//         } else {
-//             customImageView.backgroundColor = [UIColor purpleColor];
-//         }
-//     }];
-//
-//    //TRY TO SET THIS HEADERVIEW TO "STREAM_BLUR_IMAGE"
-//    UIView *headerView = [[UIView alloc] initWithFrame:CGRectMake(0, 0, frame.size.width, frame.size.height)];
-//    headerView.alpha = .7;
-//    [headerView addSubview:title];
-//    [headerView addSubview:timeInterval];
-//    [headerView addSubview:customImageView];
-//    headerView.backgroundColor = [UIColor blackColor];
-//
-//    return headerView;
-//}
-
-//- (void)tapTap:(UITapGestureRecognizer *)tapGestureRecognizer
-//{
-//    UIImageView *sender = (UIImageView *)tapGestureRecognizer.view;
-//
-//    PFObject *object = [self.pictureAndVideoArray objectAtIndex:sender.tag];
-//    PFUser *picturePhotographer = [object objectForKey:@"photographer"];
-//
-//    PFObject *like = [PFObject objectWithClassName:@"LikeActivity"];
-//    like[@"fromUser"] = [PFUser currentUser];
-//    like[@"toUser"] = picturePhotographer;
-//    like[@"photo"] = object;
-//    [like saveInBackground];
-//
-//    StreamTableViewCell *cell = (id)[self.tableView cellForRowAtIndexPath:[NSIndexPath indexPathForRow:0 inSection:sender.tag]];
-//
-//    UIImage *btnImage = [UIImage imageNamed:@"like_selected"];
-//    [cell.likeButton setImage:btnImage forState:UIControlStateNormal];
-//
-//    //increment count
-//    NSString *numberOfLikesString = cell.numberOfLikesLabel.text;
-//    NSInteger numberOfLikesInt = [numberOfLikesString integerValue];
-//    numberOfLikesInt++;
-//    cell.numberOfLikesLabel.text = [NSString stringWithFormat:@"%li likes", (long)numberOfLikesInt];
-//
-//    cell.likedImageView.hidden = NO;
-//
-//    cell.likedImageView.alpha = 0;
-//
-//    [UIView animateWithDuration:0.3 animations:^{
-//        cell.likedImageView.alpha = 1;
-//    } completion:^(BOOL finished) {
-//        [UIView animateKeyframesWithDuration:0.3 delay:0.75 options:0 animations:^{
-//            cell.likedImageView.alpha = 0;
-//        } completion:^(BOOL finished) {
-//            cell.likedImageView.hidden = YES;
-//        }];
-//    }];
-//}
