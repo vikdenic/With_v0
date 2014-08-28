9254037aebd147bf5b36491b509a94f87f8cb067
vikdenic <vik.denic@gmail.com>
2014-08-28 14:46:36 -0500

    converting homeviewcontroller to swift

---
+++ With_v0/HomeViewController.swift
@@ -0,0 +1,163 @@
+//
+//  HomeViewController.swift
+//  With_v0
+//
+//  Created by Vik Denic on 8/28/14.
+//  Copyright (c) 2014 Mobile Makers. All rights reserved.
+//
+
+import UIKit
+
+//extension Array{
+//    func appendAllObjectsFromArray(array : [AnyObject]) -> [AnyObject]
+//    {
+//        for object in array
+//        {
+//            array.append(object)
+//        }
+//
+//        return array
+//    }
+//}
+
+class HomeViewController: UIViewController, UITableViewDataSource, UITableViewDelegate, UIActionSheetDelegate {
+
+    var event = PFObject()
+
+    @IBOutlet var tableView: UITableView!
+
+    var refreshControl = UIRefreshControl()
+
+    var eventArray : [PFObject] = []
+    var indexPathArray : [NSIndexPath] = []
+    var doingTheQuery = Bool()
+
+    var originalFrame = CGRect()
+
+    var startContentOffset = CGFloat()
+    var lastContentOffset = CGFloat()
+    var hidden = Bool()
+
+    override func viewDidLoad()
+    {
+        super.viewDidLoad()
+
+        originalFrame = tabBarController.tabBar.frame
+
+        tabBarController.tabBar.tintColor = UIColor.greenColor()
+
+        var currentUser = PFUser.currentUser()
+
+        if currentUser != nil
+        {
+            queryForEvents()
+        }
+        else
+        {
+            performSegueWithIdentifier("showLogin", sender: self)
+        }
+
+        //pull to refresh
+        refreshControl = UIRefreshControl()
+        refreshControl.addTarget(self, action: "refresh", forControlEvents: UIControlEvents.ValueChanged)
+        tableView.addSubview(refreshControl)
+
+        //TODO: finish viewcontroller
+        
+    }
+
+    //MARK: Helpers
+    func queryForEvents()
+    {
+        let relation = PFUser.currentUser().relationForKey("eventsAttending")
+        let query = relation.query()
+        query.includeKey("creator")
+        query.limit = 4
+
+        if eventArray.count == 0
+        {
+            query.skip = 0
+        }
+        else{
+            query.skip = eventArray.count
+        }
+
+        query.findObjectsInBackgroundWithBlock { (objects, error) -> Void in
+
+            self.doingTheQuery = true
+
+            if self.eventArray.count < 3
+            {
+//                self.eventArray.append(<#newElement: T#>)
+                self.eventArray = objects as [PFObject]
+                self.tableView.reloadData()
+            }
+            else if self.eventArray.count >= 3
+            {
+                var theCount = self.eventArray.count
+                self.eventArray = objects as [PFObject]
+
+                for var i = theCount; i <= self.eventArray.count-1; i++
+                {
+                    var indexPath = NSIndexPath(forRow: i, inSection: 0)
+                    self.indexPathArray.append(indexPath)
+                }
+
+                self.tableView.insertRowsAtIndexPaths(self.indexPathArray, withRowAnimation: UITableViewRowAnimation.Fade)
+                self.indexPathArray.removeAll(keepCapacity: false)
+                self.tableView.reloadData()
+            }
+            self.doingTheQuery = false
+        }
+    }
+
+
+    //MARK: TableView
+    func tableView(tableView: UITableView!, numberOfRowsInSection section: Int) -> Int
+    {
+        return 1
+    }
+
+    func tableView(tableView: UITableView!, cellForRowAtIndexPath indexPath: NSIndexPath!) -> UITableViewCell!
+    {
+        var cell = tableView.dequeueReusableCellWithIdentifier("Cell") as HomeTableViewCell
+
+        return cell
+    }
+
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ With_v0/With_v0-Bridging-Header.h
@@ -0,0 +1,15 @@
+//
+//  Use this file to import your target's public headers that you would like to expose to Swift.
+//
+
+#import <Parse/Parse.h>
+
+#import "HomeTableViewCell.h"
+#import "IndividualEventViewController.h"
+#import "PageViewController.h"
+#import "LoginViewController.h"
+
+#import "UITabBarController+hidable.h"
+
+#import <QuartzCore/QuartzCore.h>
+#import <CoreImage/CoreImage.h>
